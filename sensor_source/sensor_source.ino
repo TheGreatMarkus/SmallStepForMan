bebfdb36d21241ba9e5c542ce28bbc4d7fadd8ca
TheGreatMarkus <aldea.cristian@outlook.com>
2019-07-21 18:21:06 -0400

    Added led control

--- sensor_source/sensor_source.ino
+++ sensor_source/sensor_source.ino
@@ -1,14 +1,20 @@
+
+const int LED_PIN = 13;
+const int SENSOR_PIN = 2;
+
 void setup() {
   pinMode(2, INPUT);
+  pinMode(LED_PIN, OUTPUT);
   Serial.begin(9600);
-
 }
 
 void loop() {
   if (digitalRead(2) == HIGH) {
     Serial.println("Sensor Touched");
+    digitalWrite(LED_PIN, HIGH);
   } else {
     Serial.println("Sensor not Touched");
+    digitalWrite(LED_PIN, LOW);
   }
-  delay(200);
+  delay(100);
 }
